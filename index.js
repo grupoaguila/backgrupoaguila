37fbaf3ea0f49aaaa66a98ae0331846a24a1dcba
grupoaguila <119085653+grupoaguila@users.noreply.github.com>
2023-05-18 20:43:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {PORT} = process.env;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 
 
   server.listen(PORT, () => {
