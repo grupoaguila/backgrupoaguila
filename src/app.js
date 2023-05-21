3d592a4adaf3b2f28e4ec078548e4effb56c54ae
gabdala17 <gisella.abdala@gmail.com>
2023-05-21 14:04:05 -0300

    le di acceso en app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 const routes = require('./routes/index.js');
 
-require('./db.js');
+require('./db.js'); 
 
 const server = express();
 
